d1ce1fb8d8d05e37c7e4853e6480a71073812ac4
HappyDonuts <javi_morales@hotmail.es>
2022-03-30 19:43:07 +0200

    PCB rev 1.0 added. Minor modifications to the libraries and schematics

--- Firmware_STM32CubeIDE/L031_Blink/Core/Src/main.c
+++ Firmware_STM32CubeIDE/L031_Blink/Core/Src/main.c
@@ -156,10 +156,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PA9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
